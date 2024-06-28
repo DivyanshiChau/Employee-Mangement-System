2bb2e972224488b817be1f9b8d543c5519f20947
tanu-2568 <ttanurajput2568@gmail.com>
2024-06-28 21:16:34 +0530

    Add files via upload

---
+++ ViewLeave.java
@@ -0,0 +1,120 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import javax.swing.table.DefaultTableModel;
+
+public class ViewLeave extends JFrame implements ActionListener {
+
+    JTable table;
+    Choice choiceEMP;
+    JButton searchbtn, print, back;
+
+    ViewLeave() {
+        getContentPane().setBackground(new Color(122, 140, 255));
+        setLayout(null);
+
+        JLabel search = new JLabel("Search by employee id");
+        search.setBounds(20, 20, 220, 20);
+        search.setFont(new Font("SERIF", Font.BOLD, 21));
+        add(search);
+
+        choiceEMP = new Choice();
+        choiceEMP.setBounds(270, 20, 150, 20);
+        add(choiceEMP);
+
+        try {
+            conn c = new conn();
+            ResultSet resultSet = c.statement.executeQuery("select * from employee");
+            while (resultSet.next()) {
+                choiceEMP.add(resultSet.getString("empId"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        table = new JTable();
+        try {
+            conn c = new conn();
+            ResultSet resultSet = c.statement.executeQuery("select * from employee_leave");
+            table.setModel(resultSetToTableModel(resultSet));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JScrollPane jp = new JScrollPane(table);
+        jp.setBounds(0, 100, 900, 600);
+        jp.setFont(new Font("SERIF", Font.BOLD, 15));
+        add(jp);
+
+        searchbtn = new JButton("Search");
+        searchbtn.setBounds(20, 70, 80, 20);
+        searchbtn.addActionListener(this);
+        add(searchbtn);
+
+        print = new JButton("Print");
+        print.setBounds(120, 70, 80, 20);
+        print.addActionListener(this);
+        add(print);
+
+        back = new JButton("Back");
+        back.setBounds(220, 70, 80, 20);
+        back.addActionListener(this);
+        add(back);
+
+        setSize(900, 700);
+        setLocation(300, 100);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchbtn) {
+            String query = "select * from employee_leave where emp_id = '" + choiceEMP.getSelectedItem() + "'";
+            try {
+                conn c = new conn();
+                ResultSet resultSet = c.statement.executeQuery(query);
+                table.setModel(resultSetToTableModel(resultSet));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else if (e.getSource() == print) {
+            try {
+                table.print();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else if (e.getSource() == back) {
+            setVisible(false);
+            new Main_class();  // Assuming Main_class is the class to return to main menu
+        }
+    }
+
+    public static void main(String[] args) {
+        new ViewLeave();
+    }
+
+    private DefaultTableModel resultSetToTableModel(ResultSet rs) throws Exception {
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        DefaultTableModel model = new DefaultTableModel();
+
+        for (int i = 1; i <= columnCount; i++) {
+            model.addColumn(metaData.getColumnLabel(i));
+        }
+
+        while (rs.next()) {
+            Object[] row = new Object[columnCount];
+            for (int i = 1; i <= columnCount; i++) {
+                row[i - 1] = rs.getObject(i);
+            }
+            model.addRow(row);
+        }
+
+        return model;
+    }
+}
